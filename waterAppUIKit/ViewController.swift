293b7445ecbfefb58cd7e1a1ecbe3dd2e3fe2fe9
Nikolai Trukhin <n.trukhin@uzum.com>
2022-12-20 21:24:17 +0100

    Initial Commit

---
+++ waterAppUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  waterAppUIKit
+//
+//  Created by Nikolay Truhin on 20/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
